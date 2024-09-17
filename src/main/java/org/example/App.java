391f2135948e8bff660b6061d7f2cc84b09dbbab
SR752 <stelle83@gmail.com>
2024-09-17 18:50:20 +0200

    meny töötab. VÄLJA ARVATUD EXIT
    ja soritning
    
    .....
    ei tööta arvutused!!!

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -6,8 +6,69 @@ import java.util.Scanner;
 
 public class App {
 
+    public static final int HOURS = 24;
+    public static int[] perHour = new int[HOURS];
+    public static final String[] times = {"00-01", "01-02", "02-03", "03-04",
+            "04-05", "05-06", "06-07", "07-08",
+            "08-09", "09-10", "10-11", "11-12",
+            "12-13", "13-14", "14-15", "15-16",
+            "16-17", "17-18", "18-19", "19-20",
+            "20-21", "21-22", "22-23", "23-00"};
+
+
     public static void main(String[] args) {
+
         Scanner scanner = new Scanner(System.in);
+        String menuChoice;
+
+
+        do {
+//            --> SKRIV UT MENU <--
+            printMenu();
+            menuChoice = scanner.nextLine().trim().toLowerCase();
+
+            switch (menuChoice) {
+                case "1":
+                    inputPrices();
+                    break;
+                case "2":
+                    calcMinMaxAverage();
+                    ;
+                    break;
+                case "3":
+                    inputPrices();
+                    break;
+                case "4":
+                    best4hours();;
+                    break;
+                case "e":
+                    System.out.println("closing program!");
+                    break;
+                default:
+                    System.out.println("invalid choice");
+            }
+        } while (menuChoice != "e");
+
+        scanner.close();
+    }
+
+
+//            String program = switch (menuChoice) {
+//                case "1" -> inputPrices();
+//                default -> 0;
+//            };
+//            System.out.println(program);
+//
+//
+//        }
+//
+////        inputPrices();
+////        calcMinMaxAverage();
+////        best4hours();
+//    }
+
+    public static void printMenu() {
+//        Scanner scanner = new Scanner(System.in);
         String t = """
                 Elpriser
                 ========
@@ -19,16 +80,11 @@ public class App {
                 """;
 
         System.out.println(t);
+    }
 
-//        String i = readFromKeyboard(scanner);
-//
-//
-//
-//    public static int readFromKeyboard(Scanner scanner) {
-//        if (i == "e")
-//            return scanner.nextInt();
-
-//        INMATNING ELPRISERNA
+    //        1. INMATNING ELPRISERNA
+    public static void inputPrices() {
+        Scanner scanner = new Scanner(System.in);
         String[] times = {"00-01", "01-02", "02-03", "03-04", "04-05", "05-06", "06-07", "07-08", "08-09", "09-10", "10-11", "11-12", "12-13", "13-14", "14-15", "15-16", "16-17", "17-18", "18-19", "19-20", "20-21", "21-22", "22-23", "23-00"};
         int[] perHour = new int[times.length];
 
@@ -36,10 +92,11 @@ public class App {
             System.out.print("Pris för timmen " + times[i] + ": ");
             perHour[i] = scanner.nextInt();
         }
+    }
 
 
-
-//        MIN MAX MEDEL PRIS
+    //        2. MIN MAX MEDEL PRIS
+    public static void calcMinMaxAverage() {
 
         int max_price = perHour[0];
         int min_price = perHour[0];
@@ -61,25 +118,24 @@ public class App {
         System.out.println("        Lägsta pris: " + min_index + ", " + min_price + " öre/kWh");
         System.out.println("        Högsta pris: " + max_index + ", " + max_price + " öre/kWh");
 
-// calculate sum
+        // calculate sum
         double sum = 0;
 
         for (int i = 0; i < perHour.length; i++) {
             sum += perHour[i];
         }
 
-//Calculate avarage, change to SE format
+        //Calculate avarage, change to SE format
         double average = sum / (perHour.length);
 
         System.out.print("        Medelpris: ");
         System.out.printf(Locale.of("sv", "SE"), "%.2f", average);
         System.out.println(" öre/kWh");
+    }
 
 
-// 4 tunni keskmine!!!
-// liidu kokku 4 numbrit -- for loop, mida tegime paaride puhul
-/// otsi miinimum nendest
-//        UUS ARRAY????
+// 4. 4 tunni keskmine!!! liidu kokku 4 numbrit -- for loop, mida tegime paaride puhul
+    public static void best4hours() {
 
         String[] fourHours = {"00-04", "01-05", "02-06", "03-07", "04-08", "05-09", "06-10", "07-11", "08-12", "09-13", "10-14", "11-15", "12-16", "13-17", "14-18", "15-19", "16-20", "17-21", "18-22", "19-23", "20-00"};
         int[] bestTimes = new int[fourHours.length];
@@ -107,6 +163,5 @@ public class App {
         System.out.printf(Locale.of("sv", "SE"), "%.2f", averageFour);
         System.out.println(" öre/kWh");
 //        System.out.printf("Medelpris 4h: %.2f öre" + averageFour) ;
-
     }
 }
